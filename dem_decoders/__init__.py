60eb3d4620b59f06e9f46676e66daa1635dba7f8
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-08-21 22:20:45 +0200

    Add `BP_UF` (#11)

--- dem_decoders/__init__.py
+++ dem_decoders/__init__.py
@@ -2,5 +2,6 @@ from . import util
 from .transformations import dem_to_hplc, hplc_to_dem
 from .bposd import BP_OSD
 from .bplsd import BP_LSD
+from .bpuf import BP_UF
 
-__all__ = ["util", "dem_to_hplc", "hplc_to_dem", "BP_OSD", "BP_LSD"]
+__all__ = ["util", "dem_to_hplc", "hplc_to_dem", "BP_OSD", "BP_LSD", "BP_UF"]
