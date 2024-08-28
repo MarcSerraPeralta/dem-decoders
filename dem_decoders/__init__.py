3e73e7343d42132adb61e2e8afc875b467dbb50e
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-08-29 00:10:33 +0200

    Add scanner for `BP_OSD` parameters (#19)

--- dem_decoders/__init__.py
+++ dem_decoders/__init__.py
@@ -1,7 +1,15 @@
-from . import util
+from . import util, scanners
 from .transformations import dem_to_hplc, hplc_to_dem
 from .bposd import BP_OSD
 from .bplsd import BP_LSD
 from .bpuf import BP_UF
 
-__all__ = ["util", "dem_to_hplc", "hplc_to_dem", "BP_OSD", "BP_LSD", "BP_UF"]
+__all__ = [
+    "util",
+    "scanners",
+    "dem_to_hplc",
+    "hplc_to_dem",
+    "BP_OSD",
+    "BP_LSD",
+    "BP_UF",
+]
